bb77581fd30aed49db2bd7aac4259f7c47e4b19a
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-18 13:46:14 +0300

    Update logo_and_title_widget.dart

--- lib/features/auth/presentation/widgets/logo_and_title_widget.dart
+++ lib/features/auth/presentation/widgets/logo_and_title_widget.dart
@@ -9,12 +9,18 @@ class LogoAndTitleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final spToPx = MediaQuery.of(context).devicePixelRatio;
+    print(spToPx);
+    print('hello');
     return Column(
       children: [
         AppLogoWidget(),
-        SizedBox(height: MediaQuery.of(context).size.height / 67.666667),
-        Text('اكتشف افضل وصفات الطعام',
-            style: TextStyles.logoTitle),
+        SizedBox(height: screenHeight / 67.666667),
+        Text(
+          'اكتشف افضل وصفات الطعام',
+          style: TextStyles.logoTitle,
+        ),
       ],
     );
   }
