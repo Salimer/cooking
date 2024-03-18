388dbd5fd5935622de1a6fe06ce38e2a4cb4c7be
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-18 13:07:30 +0300

    Create splash_view.dart

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,22 @@
+import 'package:cooking/core/widgets/app_logo_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:cooking/gen/assets.gen.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Image.asset(
+          Assets.images.splashBackground.path,
+          fit: BoxFit.cover,
+        ),
+        Center(child: AppLogoWidget())
+      ],
+    ));
+  }
+}
