08a813a0cbebd8d0c312de88f46f974692fdb4ff
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-18 13:07:15 +0300

    Create app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+const MaterialColor green = MaterialColor(
+  0xff15be77,
+  <int, Color>{
+    500: Color(0xff15be77),
+    100: Color(0xff53e88b),
+  },
+);
+
+const MaterialColor red = MaterialColor(
+  0xffda6317,
+  <int, Color>{
+    500: Color(0xffda6317),
+    400: Color(0xffe38751),
+    200: Color(0xffe6a986),
+    100: Color(0xffe3cbbc),
+  },
+);
+
+const MaterialColor orange = MaterialColor(
+  0xffffad1d,
+  <int, Color>{
+    500: Color(0xffffad1d),
+    400: Color(0xffffc668),
+    200: Color(0xffffdea4),
+    100: Color(0xfffef8e0),
+  },
+);
+
+// TextStyle(
+//   color: primaryColor[500];
+// )
