416b3300d5f157e3932980dc6f0cc1e69271d145
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-19 14:58:49 +0300

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'core/config/config.dart';
+import 'features/auth/presentation/views/views.dart';
 
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: AppTheme.lightTheme,
+      home: const Directionality(
+        textDirection: TextDirection.rtl,
+        child: RegisterView(),
+      ),
+    );
+  }
+}
