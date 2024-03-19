74026805da992398beb739f8895755777c2ae083
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-19 14:59:01 +0300

    Update login_view.dart

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -21,55 +21,52 @@ class LoginView extends StatelessWidget {
             child: Padding(
               padding:
                   EdgeInsets.symmetric(horizontal: authScreensPadding(context)),
-              child: Directionality(
-                textDirection: TextDirection.rtl,
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      SizedBox(height: screenH(context) * 0.25),
-                      LogoAndTitleWidget(),
-                      SizedBox(height: screenH(context) / 14),
-                      FormInputWidget(
-                        iconName: Assets.icons.emailInput,
-                        placeholder: 'البريد اللإلكتروني',
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    SizedBox(height: screenH(context) * 0.25),
+                    LogoAndTitleWidget(),
+                    SizedBox(height: screenH(context) / 14),
+                    FormInputWidget(
+                      iconName: Assets.icons.emailInput,
+                      placeholder: 'البريد اللإلكتروني',
+                    ),
+                    SizedBox(height: screenH(context) / 50),
+                    FormInputWidget(
+                        iconName: Assets.icons.passwordInput,
+                        placeholder: 'كلمة المرور'),
+                    SizedBox(height: 12),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        RememberMeCheckboxWidget(
+                            text:
+                                Text('تذكرني', style: TextStyles.rememberMe)),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Text('نسيت كلمة المرور؟',
+                              style: TextStyles.rememberMe),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 40),
+                    CustomElevatedButton(
+                      borderRadius: BorderRadius.circular(24),
+                      onPressed: () {},
+                      child: Text(
+                        'تسجيل الدخول',
+                        style: TextStyles.greenButton,
                       ),
-                      SizedBox(height: screenH(context) / 50),
-                      FormInputWidget(
-                          iconName: Assets.icons.passwordInput,
-                          placeholder: 'كلمة المرور'),
-                      SizedBox(height: 12),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          RememberMeCheckboxWidget(
-                              text:
-                                  Text('تذكرني', style: TextStyles.rememberMe)),
-                          GestureDetector(
-                            onTap: () {},
-                            child: Text('نسيت كلمة المرور؟',
-                                style: TextStyles.rememberMe),
-                          )
-                        ],
-                      ),
-                      SizedBox(height: 40),
-                      CustomElevatedButton(
-                        borderRadius: BorderRadius.circular(24),
-                        onPressed: () {},
-                        child: Text(
-                          'تسجيل الدخول',
-                          style: TextStyles.greenButton,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      AuthBottomActionWidget(
-                        greenText: 'إنشاء حساب',
-                        whiteText: 'لا تملك حساب؟',
-                      ),
-                      SizedBox(height: 30)
-                    ],
-                  ),
+                    ),
+                    SizedBox(height: 20),
+                    AuthBottomActionWidget(
+                      greenText: 'إنشاء حساب',
+                      whiteText: 'لا تملك حساب؟',
+                    ),
+                    SizedBox(height: 30)
+                  ],
                 ),
               ),
             ),
