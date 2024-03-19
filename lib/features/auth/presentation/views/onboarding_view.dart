71d4b546ea59984b78e31a509c2c7bb2b042399b
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-19 12:19:42 +0300

    Update onboarding_view.dart

--- lib/features/auth/presentation/views/onboarding_view.dart
+++ lib/features/auth/presentation/views/onboarding_view.dart
@@ -1,14 +1,7 @@
 import 'package:cooking/core/config/config.dart';
-import 'package:cooking/core/constants/app_padds.dart';
 import 'package:cooking/core/constants/constants.dart';
-import 'package:cooking/core/widgets/app_logo_widget.dart';
-import 'package:cooking/features/auth/presentation/widgets/auth_background_widget.dart';
-import 'package:cooking/features/auth/presentation/widgets/auth_bottom_action_widget.dart';
-import 'package:cooking/features/auth/presentation/widgets/custom_elevated_button.dart';
-import 'package:cooking/features/auth/presentation/widgets/logo_and_title_widget.dart';
+import 'package:cooking/features/auth/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:cooking/gen/assets.gen.dart';
-import 'package:flutter/widgets.dart';
 
 class OnboardingView extends StatelessWidget {
   const OnboardingView({super.key});
@@ -21,15 +14,16 @@ class OnboardingView extends StatelessWidget {
           const AuthBackgroundWidget(),
           SingleChildScrollView(
             child: Container(
+              width: screenW(context),
+              height: screenH(context),
               padding:
                   EdgeInsets.symmetric(horizontal: authScreensPadding(context)),
-              width: double.infinity,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   LogoAndTitleWidget(),
                   SizedBox(
-                      height: screenH(context) / 10.8266667),
+                      height: screenH(context) / 12),
                   CustomElevatedButton(
                     borderRadius: BorderRadius.circular(24),
                     onPressed: () {},
@@ -54,7 +48,7 @@ class OnboardingView extends StatelessWidget {
                     ),
                   ),
                   SizedBox(
-                      height: screenH(context) / 13.7627119),
+                      height: screenH(context) / 15),
                   const AuthBottomActionWidget(),
                   SizedBox(height: screenH(context) / 19.3333)
                 ],
