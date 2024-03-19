c371fe09355f29a13bd4c74d6084b51ed9de9bd9
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-19 14:01:30 +0300

    Update remember_me_checkbox_widget.dart

--- lib/features/auth/presentation/widgets/remember_me_checkbox_widget.dart
+++ lib/features/auth/presentation/widgets/remember_me_checkbox_widget.dart
@@ -1,11 +1,11 @@
 import 'package:cooking/core/config/config.dart';
+import 'package:cooking/core/constants/constants.dart';
 import 'package:cooking/features/auth/presentation/widgets/custom_checkbox_widget.dart';
 import 'package:flutter/material.dart';
 
 class RememberMeCheckboxWidget extends StatefulWidget {
-  const RememberMeCheckboxWidget({
-    super.key,
-  });
+  final Widget text;
+  const RememberMeCheckboxWidget({super.key, required this.text});
 
   @override
   State<RememberMeCheckboxWidget> createState() =>
@@ -20,18 +20,18 @@ class _RememberMeCheckboxWidgetState extends State<RememberMeCheckboxWidget> {
     return Row(
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment:
-          CrossAxisAlignment.start, // Control the width of the row
+          CrossAxisAlignment.center, // Control the width of the row
       children: [
         CustomCheckboxWidget(
           value: rememberMe,
           onChanged: (value) => setState(() => rememberMe = value),
           size: 25.0, // Adjust size if needed
           borderRadius: 10.0, // Adjust border radius if needed
-          activeColor: Colors.orangeAccent, // Adjust color if needed
+          activeColor: orange[500]!, // Adjust color if needed
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: const Text('تذكرني', style: TextStyles.rememberMe),
+          child: widget.text,
         ),
       ],
     );
