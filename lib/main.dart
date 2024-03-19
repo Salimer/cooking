16af13823562ad8efc789e2e5a0328ab05ba6f9c
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-19 14:59:17 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
-import 'package:cooking/core/config/app_theme.dart';
-import 'package:cooking/features/auth/presentation/views/login_view.dart';
-import 'package:cooking/features/auth/presentation/views/onboarding_view.dart';
-import 'package:cooking/features/splash/presentation/views/splash_view.dart';
+import 'package:cooking/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: AppTheme.lightTheme,
-      home: LoginView()
-    );
-  }
-}
