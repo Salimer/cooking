87c2bb5702ad7968992ec8feb627441c32b99137
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-19 12:19:39 +0300

    Update login_view.dart

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,14 +1,16 @@
+import 'package:cooking/core/config/config.dart';
 import 'package:cooking/core/constants/constants.dart';
-import 'package:cooking/features/auth/presentation/widgets/auth_background_widget.dart';
-import 'package:cooking/features/auth/presentation/widgets/form_input_widget.dart';
-import 'package:cooking/features/auth/presentation/widgets/logo_and_title_widget.dart';
+import 'package:cooking/features/auth/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:cooking/gen/assets.gen.dart';
+
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    bool rememberMe = false;
     final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
     String? email;
     return Scaffold(
@@ -21,16 +23,45 @@ class LoginView extends StatelessWidget {
               height: screenH(context),
               padding:
                   EdgeInsets.symmetric(horizontal: authScreensPadding(context)),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    LogoAndTitleWidget(),
-                    Directionality(
-                        textDirection: TextDirection.rtl,
-                        child: FormInputWidget()),
-                  ],
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      LogoAndTitleWidget(),
+                      FormInputWidget(
+                        iconName: Assets.icons.emailInput,
+                        placeholder: 'البريد اللإلكتروني',
+                      ),
+                      SizedBox(height: 40),
+                      FormInputWidget(
+                          iconName: Assets.icons.passwordInput,
+                          placeholder: 'كلمة المرور'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          RememberMeCheckboxWidget(),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Text('نسيت كلمة المرور؟',
+                                style: TextStyles.rememberMe),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      CustomElevatedButton(
+                    borderRadius: BorderRadius.circular(24),
+                    onPressed: () {},
+                    child: Text(
+                      'تسجيل الدخول',
+                      style: TextStyles.greenButton,
+                    ),
+                  ),
+                  AuthBottomActionWidget()
+                    ],
+                  ),
                 ),
               ),
             ),
