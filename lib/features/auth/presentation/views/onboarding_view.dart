1f6aa22a43be0724749f8a53ef413f232c8e7640
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-19 13:27:34 +0300

    Update onboarding_view.dart

--- lib/features/auth/presentation/views/onboarding_view.dart
+++ lib/features/auth/presentation/views/onboarding_view.dart
@@ -13,45 +13,50 @@ class OnboardingView extends StatelessWidget {
         children: [
           const AuthBackgroundWidget(),
           SingleChildScrollView(
-            child: Container(
-              width: screenW(context),
-              height: screenH(context),
-              padding:
-                  EdgeInsets.symmetric(horizontal: authScreensPadding(context)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  LogoAndTitleWidget(),
-                  SizedBox(
-                      height: screenH(context) / 12),
-                  CustomElevatedButton(
-                    borderRadius: BorderRadius.circular(24),
-                    onPressed: () {},
-                    child: Text(
-                      'إنشاء حساب',
-                      style: TextStyles.greenButton,
-                    ),
-                  ),
-                  SizedBox(height: screenH(context) / 40.6),
-                  Container(
-                    width: double.infinity,
-                    height: screenH(context) / 13.53333,
-                    child: OutlinedButton(
+            child: Directionality(
+              textDirection: TextDirection.rtl,
+              child: Container(
+                
+                padding:
+                    EdgeInsets.symmetric(horizontal: authScreensPadding(context)),
+                child: Column(
+                  children: [
+                    SizedBox(height: screenH(context) * 0.35),
+                    LogoAndTitleWidget(),
+                    SizedBox(
+                        height: screenH(context) / 12),
+                    CustomElevatedButton(
+                      borderRadius: BorderRadius.circular(24),
                       onPressed: () {},
-                      style: OutlinedButton.styleFrom(
-                        side: BorderSide(color: orange[500]!),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(24),
-                        ),
+                      child: Text(
+                        'إنشاء حساب',
+                        style: TextStyles.greenButton,
                       ),
-                      child: Text('تخطي', style: TextStyles.transButton),
                     ),
-                  ),
-                  SizedBox(
-                      height: screenH(context) / 15),
-                  const AuthBottomActionWidget(),
-                  SizedBox(height: screenH(context) / 19.3333)
-                ],
+                    SizedBox(height: screenH(context) / 40.6),
+                    Container(
+                      width: double.infinity,
+                      height: 60,
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(color: orange[500]!),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                        ),
+                        child: Text('تخطي', style: TextStyles.transButton),
+                      ),
+                    ),
+                    SizedBox(
+                        height: screenH(context) / 15),
+                    const AuthBottomActionWidget(
+                      greenText: "تسجيل الدخول",
+                      whiteText: "هل لديك حساب؟",
+                    ),
+                    SizedBox(height: screenH(context) / 19.3333)
+                  ],
+                ),
               ),
             ),
           )
