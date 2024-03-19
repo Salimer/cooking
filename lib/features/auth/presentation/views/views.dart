361570fe4c13a34f54320c309d9219c8395baf64
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-19 16:32:11 +0300

    Update views.dart

--- lib/features/auth/presentation/views/views.dart
+++ lib/features/auth/presentation/views/views.dart
@@ -1,3 +1,5 @@
 export 'login_view.dart';
 export 'onboarding_view.dart';
 export 'register_view.dart';
+export 'reset_password_enter_email_view.dart';
+export 'reset_password_new_password_view.dart';
