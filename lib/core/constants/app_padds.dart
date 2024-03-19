446105cc0aab9cdf657a4bf43cd032c07e16e476
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-19 13:29:07 +0300

    Update app_padds.dart

--- lib/core/constants/app_padds.dart
+++ lib/core/constants/app_padds.dart
@@ -1,4 +1,4 @@
 import 'package:cooking/core/constants/screen_width_height.dart';
 import 'package:flutter/material.dart';
 
- double authScreensPadding(context) => screenW(context) / 15;
+double authScreensPadding(context) => screenW(context) / 15;
