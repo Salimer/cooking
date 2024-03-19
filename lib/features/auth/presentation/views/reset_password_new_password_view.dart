e7bb2003775bbcf97a8b8c6b3ac1eea78e1c8c90
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-19 16:32:09 +0300

    Create reset_password_new_password_view.dart

---
+++ lib/features/auth/presentation/views/reset_password_new_password_view.dart
@@ -0,0 +1,62 @@
+import 'package:cooking/core/config/config.dart';
+import 'package:cooking/core/constants/constants.dart';
+import 'package:cooking/features/auth/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:cooking/gen/assets.gen.dart';
+
+class ResetPasswordNewPasswordView extends StatelessWidget {
+  const ResetPasswordNewPasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    bool rememberMe = false;
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    String? email;
+    return Scaffold(
+      body: Stack(
+        children: [
+          const AuthBackgroundWidget(),
+          SingleChildScrollView(
+            child: Padding(
+              padding:
+                  EdgeInsets.symmetric(horizontal: authScreensPadding(context)),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    SizedBox(height: screenH(context) * 0.25),
+                    LogoAndTitleWidget(
+                        title: 'استعادة كلمة المرور',
+                        bodyText:
+                            'الرجاء ادخال كلمة المرور الجديدة لحسابك'),
+                    SizedBox(height: 20),
+                    FormInputWidget(
+                      iconName: Assets.icons.passwordInput,
+                      placeholder: 'كلمة المرور الجديدة',
+                    ),
+                    SizedBox(height: screenH(context) / 50),
+                    FormInputWidget(
+                      iconName: Assets.icons.passwordInput,
+                      placeholder: 'تأكيد كلمة المرور الجديدة',
+                    ),
+                    
+                    SizedBox(height: 40),
+                    CustomElevatedButton(
+                      borderRadius: BorderRadius.circular(24),
+                      onPressed: () {},
+                      child: Text(
+                        'تأكيد',
+                        style: TextStyles.greenButton,
+                      ),
+                    ),
+                    
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
