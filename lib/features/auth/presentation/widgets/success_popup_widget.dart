e2a9ce8c0ef9dbf882747611e801cd15d8b08673
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-20 13:36:27 +0300

    Update success_popup_widget.dart

--- lib/features/auth/presentation/widgets/success_popup_widget.dart
+++ lib/features/auth/presentation/widgets/success_popup_widget.dart
@@ -11,35 +11,52 @@ class SuccessPopupWidget extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
           children: [
-            Positioned(
-              bottom: -100,
-              child: SvgPicture.asset(
-                Assets.icons.successPopupIcon,
-                semanticsLabel: 'Success popup icon',
-              ),
-            ),
             Container(
               decoration: BoxDecoration(
-                color: Colors.white,
-              ),
+                  color: Colors.white, borderRadius: BorderRadius.circular(11)),
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.only(
+                    top: 20, bottom: 30, right: 40, left: 40),
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    SizedBox(height: 200),
-                    Text('مبروك تم اعادة تعيين كلمة المرور',
-                        style: TextStyles.logoTitle),
-                    Text('سوف يتم اعادة توجيهك الى الصفحة الرئيسية خلال ',
-                        style: TextStyles.resetPasswordBody)
+                    SizedBox(height: 100),
+                    SizedBox(
+                      width: 180,
+                      child: Text(
+                        'مبروك تم اعادة تعيين كلمة المرور',
+                        style: TextStyles.logoTitle,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: 200,
+                      child: Text(
+                        'سوف يتم اعادة توجيهك الى الصفحة الرئيسية خلال ',
+                        style: TextStyles.resetPasswordBody,
+                        textAlign: TextAlign.center,
+                      ),
+                    )
                   ],
                 ),
               ),
             ),
-            
+            Container(
+              height: 290,
+              decoration: BoxDecoration(color: Colors.transparent),
+              child: Column(
+                children: [
+                  SvgPicture.asset(
+                    Assets.icons.successPopupIcon,
+                    semanticsLabel: 'Success popup icon',
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
