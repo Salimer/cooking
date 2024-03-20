b1bbf216fcb591d110bf97eb74013be69497ddd5
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-20 14:00:32 +0300

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -13,9 +13,8 @@ class App extends StatelessWidget {
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
       home: const Directionality(
-        textDirection: TextDirection.rtl,
-        child: ResetPasswordNewPasswordView()
-      ),
+          textDirection: TextDirection.rtl,
+          child: ResetPasswordNewPasswordView()),
     );
   }
 }
