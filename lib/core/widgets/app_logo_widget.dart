675febd32a658cd5a024a208add6ecd8566d6a85
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-21 11:20:53 +0300

    Update app_logo_widget.dart

--- lib/core/widgets/app_logo_widget.dart
+++ lib/core/widgets/app_logo_widget.dart
@@ -4,14 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class AppLogoWidget extends StatelessWidget {
+  final String iconPath;
   const AppLogoWidget({
     super.key,
+    required this.iconPath
   });
 
   @override
   Widget build(BuildContext context) {
     return SvgPicture.asset(
-      Assets.icons.logo,
+      iconPath,
       semanticsLabel: 'App Logo',
       height: screenH(context) / 6.6557377,
     );
