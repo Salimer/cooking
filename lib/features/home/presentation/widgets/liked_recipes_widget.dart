ef3ae75e485970b92c0b763a45977787a04f5a0e
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-24 16:03:25 +0300

    Create liked_recipes_widget.dart

---
+++ lib/features/home/presentation/widgets/liked_recipes_widget.dart
@@ -0,0 +1,41 @@
+import 'package:cooking/core/constants/constants.dart';
+import 'package:cooking/features/home/data/fake.dart';
+import 'package:cooking/features/home/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:infinite_carousel/infinite_carousel.dart';
+
+class LikedRecipesWidget extends StatelessWidget {
+  const LikedRecipesWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.only(top: 10, bottom: 10),
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: SubtitlesWidget(subtitle: 'وصفات محبوبة'),
+          ),
+          SizedBox(
+            height: 169,
+            child: InfiniteCarousel.builder(
+              loop: false,
+              itemCount: categories.length,
+              itemExtent: 327,
+              itemBuilder: (context, itemIndex, realIndex) =>
+                  Container(
+                    decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(16)),
+                    margin: EdgeInsets.all(5)
+                  ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
