5c0428ca0021d212a8c206a17cfd49a2e2d57974
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-24 14:40:43 +0300

    Update home_view.dart

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,6 +1,9 @@
+import 'package:cooking/core/config/config.dart';
 import 'package:cooking/core/constants/constants.dart';
+import 'package:cooking/features/home/data/fake.dart';
 import 'package:cooking/features/home/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:cooking/gen/assets.gen.dart';
 
@@ -10,42 +13,54 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Neutral.grey4,
-          leading: Builder(
-            builder: (context) => IconButton(
-              onPressed: () => Scaffold.of(context).openDrawer(),
-              icon: SvgPicture.asset(Assets.icons.drawerIcon),
-            ),
+      appBar: AppBar(
+        backgroundColor: Neutral.grey4,
+        leading: Builder(
+          builder: (context) => IconButton(
+            onPressed: () => Scaffold.of(context).openDrawer(),
+            icon: SvgPicture.asset(Assets.icons.drawerIcon),
           ),
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: SvgPicture.asset(Assets.icons.notificationsIcon))
-          ],
         ),
-        drawer: Drawer(),
-        body: Stack(
-          children: [
-            Column(
-              children: [
-                Container(
-                  color: Neutral.grey4,
-                  width: screenW(context),
-                  padding:
-                      EdgeInsets.only(top: 0, bottom: 40, right: 20, left: 90),
-                  child: Text('البحث عن أفضل الوصفات للطبخ',
-                      style: Theme.of(context).textTheme.headlineLarge),
-                ),
-                Container(
-                    color: Colors.white,
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    child: Column(
-                      children: [SubtitlesWidget(subtitle: 'فئات شعبية')],
-                    ))
-              ],
-            )
-          ],
-        ));
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(Assets.icons.notificationsIcon))
+        ],
+      ),
+      drawer: Drawer(),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                color: Neutral.grey4,
+                width: screenW(context),
+                padding:
+                    EdgeInsets.only(top: 0, bottom: 40, right: 20, left: 90),
+                child: Text('البحث عن أفضل الوصفات للطبخ',
+                    style: Theme.of(context).textTheme.headlineLarge),
+              ),
+              PopularCategoriesWidget(),
+              PopularRecipesWidget()
+            ],
+          )
+        ],
+      ),
+    );
   }
+
+  // Wrap WrapMethod() {
+  //   return Wrap(
+  //     direction: Axis.vertical,
+  //     children: [
+  //       for (Map<String, String> category in categories)
+  //         CategoryChipWidget(
+  //           label: category['name']!,
+  //           icon: category['icon']!,
+  //         ),
+  //     ],
+  //   );
+  // }
 }
+
+
