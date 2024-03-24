a6da1b70fdfc1b1928564f9d97e0528f5ba74014
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-24 16:03:21 +0300

    Update widgets.dart

--- lib/features/home/presentation/widgets/widgets.dart
+++ lib/features/home/presentation/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'subtitles_widget.dart';
 export 'category_chip_widget.dart';
 export 'popular_categories_widget.dart';
 export 'popular_recipes_widget.dart';
+export 'liked_recipes_widget.dart';
