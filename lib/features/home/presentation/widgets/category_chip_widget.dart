c49abd7658a1c075f1eea1fa0359f0149906a467
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-24 14:40:35 +0300

    Create category_chip_widget.dart

---
+++ lib/features/home/presentation/widgets/category_chip_widget.dart
@@ -0,0 +1,40 @@
+import 'package:cooking/core/config/config.dart';
+import 'package:cooking/core/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+class CategoryChipWidget extends StatelessWidget {
+  final String label;
+  final String icon;
+  const CategoryChipWidget({
+    super.key,
+    required this.icon,
+    required this.label 
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+          margin: EdgeInsets.only(right: 20),
+          padding: EdgeInsets.only(right: 5, top: 4, bottom: 4, left: 15),
+          decoration: BoxDecoration(
+            color: Neutral.grey4,
+            borderRadius: BorderRadius.circular(27),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  padding:
+                      EdgeInsets.only(bottom: 5.5, left: 6.5, top: 3, right: 3),
+                  decoration: BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
+                  child: Text(icon, style: TextStyle(fontSize: 16))),
+              SizedBox(width: 5),
+              Text(label, style: TextStyles.chipText)
+            ],
+          )),
+    );
+  }
+}
