65a286d5a2432a8ca19e781678c67b183cff371c
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-25 14:15:24 +0300

    Update popular_recipes_widget.dart

--- lib/features/home/presentation/widgets/popular_recipes_widget.dart
+++ lib/features/home/presentation/widgets/popular_recipes_widget.dart
@@ -1,10 +1,5 @@
-import 'package:cooking/core/config/config.dart';
-import 'package:cooking/core/constants/constants.dart';
-import 'package:cooking/features/home/data/fake.dart';
 import 'package:cooking/features/home/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:infinite_carousel/infinite_carousel.dart';
 
 class PopularRecipesWidget extends StatelessWidget {
   const PopularRecipesWidget({
@@ -24,30 +19,12 @@ class PopularRecipesWidget extends StatelessWidget {
           ),
           SizedBox(
             height: 169,
-            child: InfiniteCarousel.builder(
-              loop: false,
-              itemCount: categories.length,
-              itemExtent: 327,
-              itemBuilder: (context, itemIndex, realIndex) => Container(
-                decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(16)),
-                margin: EdgeInsets.all(5),
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
-                    child: Text(
-                      'عنوان الوصفة يمكن تغيير بعنوان اخر عنوان الوصفة يمكن',
-                      style: TextStyles.paraBold.copyWith(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            child: InfiniteCarouselWidget()
           ),
         ],
       ),
     );
   }
 }
+
+
