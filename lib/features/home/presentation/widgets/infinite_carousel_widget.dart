8f6fa9715daad932dfcd64aa795b066399cbbfde
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-25 14:15:28 +0300

    Create infinite_carousel_widget.dart

---
+++ lib/features/home/presentation/widgets/infinite_carousel_widget.dart
@@ -0,0 +1,24 @@
+import 'package:cooking/core/constants/constants.dart';
+import 'package:cooking/features/home/data/fake.dart';
+import 'package:cooking/features/home/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:infinite_carousel/infinite_carousel.dart';
+
+class InfiniteCarouselWidget extends StatelessWidget {
+  const InfiniteCarouselWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InfiniteCarousel.builder(
+      center: phoneOrientation(context) == Orientation.landscape ? false : true,
+      loop: false,
+      itemCount: categories.length,
+      itemExtent: 327,
+      itemBuilder: (context, itemIndex, realIndex) {
+        return CarouselBoxWidget();
+      },
+    );
+  }
+}
