8f3af16d6555e018d018171bc00818c7e42b1cca
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-25 13:23:03 +0300

    Update change_email_widget.dart

--- lib/features/auth/presentation/widgets/change_email_widget.dart
+++ lib/features/auth/presentation/widgets/change_email_widget.dart
@@ -26,7 +26,7 @@ class ChangeEmailWidget extends StatelessWidget {
               Assets.icons.editEmailIcon,
             ),
             SizedBox(width: 5),
-            Text('aldeerah400@gmail.com', style: TextStyles.transButton)
+            Text('aldeerah400@gmail.com', style: TextStyles.paraRegular.copyWith(color: red[500]))
           ],
         ),
       ),
