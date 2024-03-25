f2d5674fce078490d11f058c4f5563e815e80c12
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-25 14:15:19 +0300

    Create carousel_box_widget.dart

---
+++ lib/features/home/presentation/widgets/carousel_box_widget.dart
@@ -0,0 +1,35 @@
+import 'package:cooking/core/config/config.dart';
+import 'package:cooking/core/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+class CarouselBoxWidget extends StatelessWidget {
+  const CarouselBoxWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(16),
+        gradient: const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xffd9d9d9), Colors.black],
+        ),
+      ),
+      margin: EdgeInsets.all(5),
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+          child: Text(
+            'عنوان الوصفة يمكن تغيير بعنوان اخر عنوان الوصفة يمكن',
+            style: TextStyles.paraBold.copyWith(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
