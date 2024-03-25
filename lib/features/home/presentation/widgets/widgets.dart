8e043723d0d97fb3408bf3c4afb0af80a855b2a3
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-25 14:15:26 +0300

    Update widgets.dart

--- lib/features/home/presentation/widgets/widgets.dart
+++ lib/features/home/presentation/widgets/widgets.dart
@@ -4,3 +4,5 @@ export 'popular_categories_widget.dart';
 export 'popular_recipes_widget.dart';
 export 'liked_recipes_widget.dart';
 export 'search_bar_widget.dart';
+export 'carousel_box_widget.dart';
+export 'infinite_carousel_widget.dart';
