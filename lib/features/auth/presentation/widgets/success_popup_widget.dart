076809a5bcd439685d199fb06655317cd841a293
Salim Bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-05 17:14:13 +0300

    Update success_popup_widget.dart

--- lib/features/auth/presentation/widgets/success_popup_widget.dart
+++ lib/features/auth/presentation/widgets/success_popup_widget.dart
@@ -1,4 +1,3 @@
-import 'package:cooking/core/config/config.dart';
 import 'package:cooking/core/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
