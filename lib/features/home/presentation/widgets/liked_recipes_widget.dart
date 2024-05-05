275134470112ba2d90b40d7e337c83bf18e237b3
Salim Bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-05 17:14:06 +0300

    Update liked_recipes_widget.dart

--- lib/features/home/presentation/widgets/liked_recipes_widget.dart
+++ lib/features/home/presentation/widgets/liked_recipes_widget.dart
@@ -1,9 +1,5 @@
-import 'package:cooking/core/constants/constants.dart';
-import 'package:cooking/features/home/data/fake.dart';
 import 'package:cooking/features/home/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:infinite_carousel/infinite_carousel.dart';
 
 class LikedRecipesWidget extends StatelessWidget {
   const LikedRecipesWidget({
@@ -14,10 +10,10 @@ class LikedRecipesWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      padding: EdgeInsets.only(top: 10, bottom: 10),
-      child: Column(
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      child: const Column(
         children: [
-          const Padding(
+          Padding(
             padding: EdgeInsets.only(left: 20, right: 20),
             child: SubtitlesWidget(subtitle: 'وصفات محبوبة'),
           ),
