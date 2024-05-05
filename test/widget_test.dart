712c2d01df3f257ed733889442a30d11a3467af2
Salim Bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-05 17:13:58 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:cooking/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cooking/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
