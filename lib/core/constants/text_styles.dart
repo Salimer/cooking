24cbf8005a6a5e6f584eb81df1ec5b651430b217
Salim Bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-05 16:49:41 +0300

    Create text_styles.dart

---
+++ lib/core/constants/text_styles.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle headingBold = TextStyle(
+    fontSize: 56,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle headingRegular = TextStyle(
+    fontSize: 56,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle h1Bold = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle h1Regular = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle h2Bold = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle h2Regular = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle h3Bold = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle h3Regular = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle h4Bold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle h4Regular = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle h5Bold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle h5Regular = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle paraBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle paraRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle labelBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle labelRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle smallBold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle smallRegular = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle tinyBold = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.bold,
+  );
+}
