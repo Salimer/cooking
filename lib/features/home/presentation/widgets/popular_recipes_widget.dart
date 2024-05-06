d1fb61aea93e12d2892089799c43711f05bd9638
Salim-bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-06 23:02:37 +0300

    Update popular_recipes_widget.dart

--- lib/features/home/presentation/widgets/popular_recipes_widget.dart
+++ lib/features/home/presentation/widgets/popular_recipes_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cooking/core/constants/constants.dart';
 import 'package:cooking/features/home/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -9,13 +10,13 @@ class PopularRecipesWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      color: AppColors.homeBackground,
       padding: const EdgeInsets.only(top: 10, bottom: 10),
       child: const Column(
         children: [
           Padding(
             padding: EdgeInsets.only(left: 20, right: 20),
-            child: SubtitlesWidget(subtitle: 'اشهر الوصفات'),
+            child: SubtitlesWidget(subtitle: 'الأكثر مشاهدة'),
           ),
           SizedBox(height: 169, child: InfiniteCarouselWidget()),
         ],
