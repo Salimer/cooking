1fd8798c077363fe362c074aaa60f77907805305
Salim Bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-07 10:12:21 +0300

    Update popular_categories_widget.dart

--- lib/features/home/presentation/widgets/popular_categories_widget.dart
+++ lib/features/home/presentation/widgets/popular_categories_widget.dart
@@ -1,9 +1,6 @@
 import 'package:cooking/core/constants/constants.dart';
 import 'package:cooking/features/home/data/fake.dart';
-import 'package:cooking/features/home/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:flutter_svg/svg.dart';
 
 class PopularCategoriesWidget extends StatelessWidget {
   const PopularCategoriesWidget({super.key});
@@ -15,36 +12,33 @@ class PopularCategoriesWidget extends StatelessWidget {
       padding: const EdgeInsets.only(top: 10, bottom: 10),
       child: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 20, right: 20),
-            child: SubtitlesWidget(subtitle: 'فئات شعبية'),
-          ),
           SizedBox(
-            height: 110,
+            height: 103,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: categories.length,
               itemBuilder: (context, index) {
                 return Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Column(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: orange[100],
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              categories[index]['icon']!,
-                              style: const TextStyle(fontSize: 30),
-                            ),
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: orange[100],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Text(
+                            categories[index]['icon']!,
+                            style: const TextStyle(fontSize: 30),
                           ),
                         ),
-                        Text(categories[index]['name']!)
-                      ],
-                    ));
+                      ),
+                      Text(categories[index]['name']!)
+                    ],
+                  ),
+                );
               },
             ),
           )
