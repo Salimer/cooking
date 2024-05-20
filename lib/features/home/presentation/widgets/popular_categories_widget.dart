c0a66fc08cb28afcfb4cee8708b32feedaa75bdc
Salim-bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-20 22:32:17 +0300

    Update popular_categories_widget.dart

--- lib/features/home/presentation/widgets/popular_categories_widget.dart
+++ lib/features/home/presentation/widgets/popular_categories_widget.dart
@@ -8,41 +8,39 @@ class PopularCategoriesWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: AppColors.homeBackground,
       padding: const EdgeInsets.only(top: 10, bottom: 10),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 103,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: orange[100],
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: Text(
-                            categories[index]['icon']!,
-                            style: const TextStyle(fontSize: 30),
-                          ),
-                        ),
+      child: SizedBox(
+        height: 103,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: orange[100],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Text(
+                        categories[index]['icon']!,
+                        style: const TextStyle(fontSize: 30),
                       ),
-                      Text(categories[index]['name']!)
-                    ],
+                    ),
                   ),
-                );
-              },
-            ),
-          )
-        ],
+                  Text(
+                    categories[index]['name']!,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  )
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
