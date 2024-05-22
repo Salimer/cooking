972d01d19d011bb4685fe639835a1197d79a1e5e
Salim-bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-22 23:16:47 +0300

    Update reset_password_new_password_view.dart

--- lib/features/auth/presentation/views/reset_password_new_password_view.dart
+++ lib/features/auth/presentation/views/reset_password_new_password_view.dart
@@ -26,7 +26,7 @@ class ResetPasswordNewPasswordView extends StatelessWidget {
           SingleChildScrollView(
             child: Padding(
               padding:
-                  EdgeInsets.symmetric(horizontal: authScreensPadding(context)),
+                  EdgeInsets.symmetric(horizontal: authScreensPadding),
               child: Form(
                 key: formKey,
                 child: Column(
