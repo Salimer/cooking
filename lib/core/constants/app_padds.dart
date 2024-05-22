2fdacde9f68b69cc737b195c4e466e02d6d30004
Salim-bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-22 23:17:03 +0300

    Update app_padds.dart

--- lib/core/constants/app_padds.dart
+++ lib/core/constants/app_padds.dart
@@ -1,4 +1,4 @@
 import 'package:cooking/core/config/media_query.dart';
 
-double authScreensPadding(context) => screenW(context) / 15;
-double homeScreenPadding(context) => screenW(context) / 16;
+double authScreensPadding = 15;
+double homeScreenPadding = 16;
