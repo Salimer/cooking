4a065956ca0ca0e2f76c45c4ddfae8e9d24dbc10
Salim-bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-22 23:17:00 +0300

    Create appbar_button_widget.dart

---
+++ lib/core/widgets/appbar_button_widget.dart
@@ -0,0 +1,27 @@
+import 'package:cooking/core/constants/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AppbarButtonWidget extends StatelessWidget {
+  final String icon;
+  final Color color;
+  const AppbarButtonWidget({super.key, required this.icon, this.color = Neutral.grey5});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
+        ),
+        child: SvgPicture.asset(
+          icon,
+          width: 25,
+        ),
+      ),
+    );
+  }
+}
