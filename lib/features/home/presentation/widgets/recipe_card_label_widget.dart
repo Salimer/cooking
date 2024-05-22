487329b1c4b37c9da78db21eb75b2fc6baaceb02
Salim Bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-22 12:17:54 +0300

    Update recipe_card_label_widget.dart

--- lib/features/home/presentation/widgets/recipe_card_label_widget.dart
+++ lib/features/home/presentation/widgets/recipe_card_label_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cooking/core/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -6,36 +7,53 @@ class RecipeCardLabelWidget extends StatelessWidget {
   final String greyText;
   final int duration;
   final String icon;
+  final double pad;
+  final Color? backgroundColor;
 
   const RecipeCardLabelWidget({
     super.key,
     required this.greyText,
     required this.redText,
     required this.duration,
-    required this.icon
+    required this.icon,
+    this.pad = 0,
+    this.backgroundColor,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SvgPicture.asset(icon),
-            const SizedBox(width: 5),
-            RichText(
-              text: TextSpan(children: [
-                TextSpan(text: duration.toString(), style: const TextStyle(color: Colors.red)),
-                const TextSpan(text: ' '),
-                TextSpan(text: redText, style: const TextStyle(color: Colors.red))
-              ]),
-            )
-          ],
-        ),
-        Text(greyText, style: const TextStyle(color: Colors.grey),)
-      ],
+    return Container(
+      padding: EdgeInsets.all(pad),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: backgroundColor,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SvgPicture.asset(icon),
+              const SizedBox(width: 5),
+              RichText(
+                text: TextSpan(children: [
+                  TextSpan(
+                      text: duration.toString(),
+                      style: const TextStyle(color: Colors.red)),
+                  const TextSpan(text: ' '),
+                  TextSpan(
+                      text: redText, style: const TextStyle(color: Colors.red))
+                ]),
+              )
+            ],
+          ),
+          Text(
+            greyText,
+            style: const TextStyle(color: Colors.grey),
+          )
+        ],
+      ),
     );
   }
 }
