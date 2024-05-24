8f2df28abdce3f89ab583e7abac15b55a8ce95d8
Salim-bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-24 14:06:12 +0300

    Delete media_query.dart

--- lib/core/config/media_query.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-double screenH(context) => MediaQuery.of(context).size.height;
-double screenW(context) => MediaQuery.of(context).size.width;
-Orientation phoneOrientation(context) => MediaQuery.of(context).orientation;
