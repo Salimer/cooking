ce93ed1447a72f1fef8ef614ccb57bc0b180ab40
Salim-bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-24 14:06:08 +0300

    Update context_extension.dart

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 extension ContextExtension on BuildContext {
   double get screenHeight => MediaQuery.of(this).size.height;
   double get screenWidth => MediaQuery.of(this).size.width;
+  Orientation get screenOrientation => MediaQuery.of(this).orientation;
 
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => Theme.of(this).textTheme;
