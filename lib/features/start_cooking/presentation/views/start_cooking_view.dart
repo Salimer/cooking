af28d19d11d91f58504ccef6fc1f807ff8214580
Salim-bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-25 00:28:21 +0300

    Update start_cooking_view.dart

--- lib/features/start_cooking/presentation/views/start_cooking_view.dart
+++ lib/features/start_cooking/presentation/views/start_cooking_view.dart
@@ -1,3 +1,4 @@
+import 'package:cooking/core/extensions/extensions.dart';
 import 'package:cooking/gen/assets.gen.dart';
 import 'package:cooking/core/constants/constants.dart';
 import 'package:cooking/core/widgets/widgets.dart';
@@ -9,48 +10,83 @@ class StartCookingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       backgroundColor: Neutral.black,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: homeScreenPadding),
-          child: Stack(
-            children: [
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      appBar: _appBar(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: homeScreenPadding),
+        child: Stack(
+          children: [
+            SizedBox(
+              width: context.screenWidth,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: ListView(children: [
+                  Stack(
                     children: [
-                      Row(
-                        children: [
-                          AppbarButtonWidget(
-                              icon: Assets.icons.arrowRight,
-                              color: Neutral.grey1),
-                          SizedBox(width: 5),
-                          Text(
-                            'طريقة التحضير',
-                            style: TextStyles.h5Bold
-                                .copyWith(color: Neutral.white),
-                          )
-                        ],
+                      Container(
+                        margin: const EdgeInsets.only(top: 25),
+                        padding:
+                            const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Neutral.grey1),
+                        child: Text(
+                          'نبدأ معا بتقطيع البصل الى ارباع ونزيل الشحم الزائد عن قطع اللحم ثم نغسلها. ونخفق البيض',
+                          style: TextStyles.paraRegular
+                              .copyWith(color: Neutral.white),
+                        ),
                       ),
-                      Text(
-                        '1/3',
-                        style: TextStyles.h5Bold.copyWith(color: Neutral.white),
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                          decoration: BoxDecoration(
+                            color: red[500],
+                            borderRadius: BorderRadius.circular(4)
+                          ),
+                          child: Text('1', style: TextStyles.paraBold.copyWith(color: Neutral.white),),
+                        ),
                       )
                     ],
                   )
-                ],
+                ]),
               ),
-              Positioned(
-                bottom: 0,
-                  child: ElevatedButton(
-                onPressed: () {},
-                child: Text('press me', style: TextStyle(color: Colors.white),),
-              ))
-            ],
-          ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: SafeArea(
+                bottom: true,
+                child: CustomElevatedButton(
+                  text: 'التالي',
+                  onPressed: () {},
+                ),
+              ),
+            )
+          ],
         ),
       ),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      centerTitle: false,
+      backgroundColor: Colors.black.withOpacity(0.6),
+      leading: AppbarButtonWidget(
+          icon: Assets.icons.arrowRight, color: Neutral.grey1),
+      title: Text(
+        'طريقة التحضير',
+        style: TextStyles.h5Bold.copyWith(color: Neutral.white),
+      ),
+      actions: [
+        Text(
+          '3/1',
+          style: TextStyles.h5Bold.copyWith(color: Neutral.white),
+        )
+      ],
     );
   }
 }
