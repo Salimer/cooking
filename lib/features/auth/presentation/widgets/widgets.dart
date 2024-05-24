45ddef5d5cdf5267351ce38753d5dc4e9fa2971b
Salim-bamahfoodh <s.bamahfoodh@bootfi.com>
2024-05-25 00:28:22 +0300

    Update widgets.dart

--- lib/features/auth/presentation/widgets/widgets.dart
+++ lib/features/auth/presentation/widgets/widgets.dart
@@ -1,7 +1,6 @@
 export 'auth_background_widget.dart';
 export 'auth_bottom_action_widget.dart';
 export 'custom_checkbox_widget.dart';
-export 'custom_elevated_button.dart';
 export 'form_input_widget.dart';
 export 'logo_and_title_widget.dart';
 export 'remember_me_checkbox_widget.dart';
